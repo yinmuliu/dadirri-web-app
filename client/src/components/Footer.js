dc9f90bcf68cbdf9b51e9cabaf53d0f7150d2196
Yinmu Liu <yinmu.liu@gmail.com>
2022-08-21 23:19:10 +1000

    complete about, footer, navbar styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,23 +1,88 @@
-import {
-  ButtonGroup,
-  Container,
-  IconButton,
-  Stack,
-  Text,
-  Image,
-  Flex,
-  Spacer,
-  Box,
-} from "@chakra-ui/react";
+import { ButtonGroup, Image, Flex, Spacer } from "@chakra-ui/react";
 import * as React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import {
+  Box,
+  chakra,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+const Logo = (props) => {
+  return (
+    <Stack direction="row">
+      <Image
+        boxSize="50px"
+        objectFit="cover"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Australian_Aboriginal_Flag.svg/800px-Australian_Aboriginal_Flag.svg.png"
+        alt="Australia Aboriginal Flag"
+      />
+      <Image
+        boxSize="50px"
+        objectFit="cover"
+        src="https://upload.wikimedia.org/wikipedia/en/thumb/1/1d/Flag_of_the_Torres_Strait_Islanders.svg/1200px-Flag_of_the_Torres_Strait_Islanders.svg.png"
+        alt="Torres Strait Islander Flag"
+      />
+      <Image
+        boxSize="50px"
+        src="https://media.them.us/photos/5b1ef721bfa1890010f0e15e/3:2/w_1079,h_719,c_limit/new-pride-flag-01.jpg"
+        alt="Rainbow Flag"
+      />
+    </Stack>
+  );
+};
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
 
 const Footer = () => {
   return (
-    <div>
-      <Container as="footer" role="contentinfo" py={{ base: "10", md: "10" }}>
-        <Stack spacing={{ base: "4", md: "4" }}>
-          <Stack>
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "4fr 2fr" }}
+          spacing={8}
+        >
+          <Stack align={"flex-start"}>
             <Text as="em" fontSize="md" color="black">
               Acknowledgement of Country
             </Text>
@@ -32,48 +97,34 @@ const Footer = () => {
               respect to all Aboriginal and Torres Strait Islander peoples
               today.
             </Text>
-            <Stack direction="row">
-              <Image
-                boxSize="50px"
-                objectFit="cover"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Australian_Aboriginal_Flag.svg/800px-Australian_Aboriginal_Flag.svg.png"
-                alt="Australia Aboriginal Flag"
-              />
-              <Image
-                boxSize="50px"
-                objectFit="cover"
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/1/1d/Flag_of_the_Torres_Strait_Islanders.svg/1200px-Flag_of_the_Torres_Strait_Islanders.svg.png"
-                alt="Torres Strait Islander Flag"
-              />
-              <Image
-                boxSize="50px"
-                src="https://media.them.us/photos/5b1ef721bfa1890010f0e15e/3:2/w_1079,h_719,c_limit/new-pride-flag-01.jpg"
-                alt="Rainbow Flag"
-              />
+          </Stack>
+          <Stack spacing={6}>
+            <Box>
+              <Logo color={useColorModeValue("gray.700", "white")} />
+            </Box>
+            <Text fontSize={"sm"}>
+              © Dadirri is made by Yinmu with respect and love, 2022
+            </Text>
+            <Stack direction={"row"} spacing={6}>
+              <ButtonGroup variant="ghost">
+                <IconButton
+                  as="a"
+                  href="#"
+                  aria-label="LinkedIn"
+                  icon={<FaLinkedin fontSize="1.5rem" />}
+                />
+                <IconButton
+                  as="a"
+                  href="#"
+                  aria-label="GitHub"
+                  icon={<FaGithub fontSize="1.5rem" />}
+                />
+              </ButtonGroup>
             </Stack>
           </Stack>
-          <Text fontSize="sm" color="subtle">
-            &copy; {new Date().getFullYear()} Dadirri is made by Yinmu with
-            respect and love, 2022
-          </Text>
-
-          <ButtonGroup variant="ghost">
-            <IconButton
-              as="a"
-              href="#"
-              aria-label="LinkedIn"
-              icon={<FaLinkedin fontSize="1.25rem" />}
-            />
-            <IconButton
-              as="a"
-              href="#"
-              aria-label="GitHub"
-              icon={<FaGithub fontSize="1.25rem" />}
-            />
-          </ButtonGroup>
-        </Stack>
+        </SimpleGrid>
       </Container>
-    </div>
+    </Box>
   );
 };
 
--- client/src/components/Homepage/WelcomeMsg.js
+++ client/src/components/Homepage/WelcomeMsg.js
@@ -1,12 +1,15 @@
+import { Heading, Stack, Text } from "@chakra-ui/react";
+
 const WelcomeMsg = () => {
   return (
-    <>
-      <h1>Welcome! username.</h1>
-      <p>
-        Click on languages on the map near your location, and listen to the
-        unique sound clips recorded on the land where the langauge is spoken.
-      </p>
-    </>
+    <Stack spacing={4} my="10" mx="10">
+      <Heading fontSize={"3xl"}>Welcome! username.</Heading>
+      <Text color={"gray.600"} fontSize={"xl"}>
+        Click on languages on the map near your location, listen to and enjoy
+        the unique sound clips recorded on the land where the langauge is
+        spoken.
+      </Text>
+    </Stack>
   );
 };
 
